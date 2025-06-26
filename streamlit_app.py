eba33ae3ae0575796132fbabcbf1c4e46e50a76f
thezeyus <32715050+thezeyus@users.noreply.github.com>
2025-06-26 15:25:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Set Streamlit app configuration
 st.set_page_config(
-    page_title="📡 LTE Network Analyzer",
+    page_title="LTE Network Analyzer",
     page_icon="📶",
 )
 
